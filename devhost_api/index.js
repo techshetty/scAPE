a550cdcfe56ad30018fc80f3b440e454a716e86b
Pratheek G Shetty <pratheekshetty934@gmail.com>
2024-10-25 23:15:46 +0530

    Revert "teamfetch api"
    This reverts commit 516962dc6767a194f8a46b33cc66c9ee5999c087.

--- devhost_api/index.js
+++ devhost_api/index.js
@@ -10,7 +10,6 @@ const {regStats} = require('./dHostStats.js')
 const QRCode = require('qrcode');
 require('dotenv').config();
 require('./auth.js');
-// require('./grp.js');
 require('./dbInit');
 const cors = require('cors');
 const app = express();
